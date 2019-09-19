dacd01359422675a45ecf3f2f2604dcc7b56d59c
rtanishkreddy <rreddy@unomaha.edu>
2019-09-19 03:25:13 -0500

    first assignment

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>"This is {{ their_name }}, To Do list developed in Vue.Js to learn Vue.Js”
+</h1>
+    <p>I loved this Tutorial!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
